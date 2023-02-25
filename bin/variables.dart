1eb5d6acf7ded997e07958823a481fb23eea236f
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-02-25 21:31:20 +0500

    Variables

---
+++ bin/variables.dart
@@ -0,0 +1,22 @@
+void main(){
+
+
+  /*Rules for variables 
+  . cannot start with any number
+  . cannot assign reserved name for example loops name, dataTypes name, print etc
+  */
+
+
+  //Valid Variables...
+  var name      = "Hamza";
+  var last_name = 'Rafique';
+  var address1  = 'Pakistan';
+  var $age      = 23;
+
+  //printing variables...
+  print(name);
+  print(last_name);
+  print(address1);
+  print($age);
+
+}
