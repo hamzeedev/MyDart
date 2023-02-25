4cb1e7b43a19e20b6403287500ba63754049cf40
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-02-26 01:11:09 +0500

    sleep time

---
+++ bin/operators.dart
@@ -0,0 +1,89 @@
+void main(){
+  //Arithmetic Operators...
+
+  /*var a = 10;
+  var b = 10;
+  var c = 50;
+  var d = 3;
+
+  print(a + b);
+  print(c - b);
+  print(a * b);
+  print(a / d);
+  print(c % b);
+*/
+
+//===============================================
+// increment/decrement operator
+
+//prefix increment & wiseversa for decrement
+
+/*it will do increment first and after print
+
+var i = 4;
+print(i);
+print(++i);
+print(i);
+
+*/
+
+//posfix increment & wiseversa for decrement
+
+/*it will first print then do increment
+
+var i = 4;
+print(i);
+print(i++);
+print(i);
+
+*/
+
+//============================================
+//equality & relational operators
+
+/*
+
+var a = 10;
+var b = 20;
+
+print(a == b);
+print(a != b);
+print(a > b);
+print(a < b);
+print(a >= b);
+print(a <= b);
+
+*/
+
+//============================================
+//logical operators...
+/*
+
+AND Operator...
+T && T = T
+T && F = F
+F && T = F
+F && F = F
+
+OR Operator...
+T || T = T
+T || F = T
+F || T = T
+F || F = F
+
+*/
+
+/*
+var a = 1;
+var b = 1;
+var c = 2;
+var d = 2;
+
+print((a == b) && (c == d));
+print((a == b) || (c == d));
+*/
+
+//============================================
+//type test operator...
+
+}
