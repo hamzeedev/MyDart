83b7267a7370df9afad4c4ee0bc9b08df6c1c185
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-02-27 01:17:12 +0500

    conditonal expression complete

---
+++ bin/conditionalExp.dart
@@ -0,0 +1,23 @@
+//conditional expressions
+void main(){
+//future hint.... Use it in login of app...
+
+
+  var is_login = true; //if there is "true" condition 1 will run, if false 2nd will run...
+  var user = is_login ? 'Welcom, Wheels on Rent' : 'Guest';
+  print(user);
+
+
+
+//es mai agr null nahi hai to ( name ) print kr day nahi to ( Guest ) print kr do
+
+
+var name = null;
+var user1 = name ?? 'Guest';
+print(user1);
+
+
+
+
+
+}
