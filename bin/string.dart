6b679e227098d4ecfea78ec723e89fe4e2067a21
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-02-27 10:40:34 +0500

    update

--- bin/string.dart
+++ bin/string.dart
@@ -13,20 +13,20 @@ void main(){
   */
 
   // expression inside string...
-  /*
-  var name = 'Hamza';
-  print('My Name is $name');
-  print('My Name is $name and im a software engineer');
+  
+  var name1 = 'Hamza';
+  print('My Name is $name1');
+  print('My Name is $name1 and im a software engineer');
   // {} these braces ar compalsory when we use built in function like example given below 
-  print('My Name is ${name.toUpperCase()}');
+  print('My Name is ${name1.toUpperCase()}');
 
-  */
+  
   //string concatination...
-  /*
+  
   var s1 = 'Hello';
   var s2 = 'Hamza';
   print(s1 + " My name is " + s2);
-  */
+  
 
   //string properties and methods...
   var name = "Hamza";
@@ -38,6 +38,7 @@ void main(){
   print(name.padLeft(20));
   print(eg);
   //to remove unnesseary spaces use trim()
+  //note :- some time user put unnessasary space in filling form there the trim() will help to maintain the form and give data with out spaces
   print(eg.trim());
   
 
