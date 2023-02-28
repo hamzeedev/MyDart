9ca6a651087331d36022bbb4102105efb57afa87
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-02-28 22:50:05 +0500

    Set comm d

--- bin/set.dart
+++ bin/set.dart
@@ -1,3 +1,46 @@
 void main(){
+  // unorder collection of unique items
+  // no doblication in SET
+  // values wil be  unique
+  /*
+  var st  = {'Hamza', 'Rafique', 'Ramay'};
+  var st1 = {4,5,'Hamza', 'Rafique', 'Ramay'};
+  //when you want to make set strickt to get specific type data
+  Set<String> st2 ={'Hamza', 'Rafique'};
+  
+  print(st);
+  print(st1);
+  print(st2);
+  print(st.runtimeType);
+  print(st1.runtimeType);
+  */
+
+
+
+
+  //## Empty set
+  /*
+  dont make empty set like this , in below
+
+  -> var st3 = {}; // it is a map
+
+  because this will make a map instead of set
+  
+  */
+  // var st3 = <int>{};
+  // print(st3);
+
+
+
+
+  // ## add data in empty set
+
+  // var p = <String>{};
+  // p.add('Flutter');
+  // print(p);
+
+
+
+  
   
 }
