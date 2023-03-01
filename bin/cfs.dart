5defd023bd5c7b6ee178db32d3e46bc524c78f5f
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-01 14:44:04 +0500

    control flow statements completed

--- bin/cfs.dart
+++ bin/cfs.dart
@@ -1,4 +1,34 @@
 //Control flow statements (if, if else etc)
 void main(){
+  // ## IF statement...
+  
+  
+ var user_login = true;
+  if (user_login){
+    print('Welcome to Wheels on Rent');
+  } 
+ 
+
+
+  // ## IF ELSE
+  var user_login1 = false;
+  if (user_login1){
+    print('Welcome to Wheels on Rent');
+  }
+  else{
+    print('Welcome as GUEST');
+  }
+
+
+
+// ## IF... ELSE IF... ELSE
+var a = 10;
+if (a==10){
+  print('value of a is $a');
+} else if (a==20){
+  print('value is $a');
+} else {
+  print('invalid value');
+}
   
 }
