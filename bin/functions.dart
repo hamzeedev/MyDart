2a48d32e2a64e958ac55529cd37f4726d2d44eb9
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-02 23:20:46 +0500

    started function

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,3 +1,10 @@
+// functions
+/* 
+-> most important topic in dart
+-> dart mai function ek object ki trha kam krta hai esi liya dart ek true object oriented language hai
+-> dart k kisi b function ko kisi b variable mai assign kr skty hain
+
+ */
 void main(){
   
 }
